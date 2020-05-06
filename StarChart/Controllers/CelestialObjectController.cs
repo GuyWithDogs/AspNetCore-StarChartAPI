53a02d48710085f29a6804e0bd6dcb7965772f3e
Warrick Wilson <guywithdogs@gmail.com>
2020-05-06 14:17:22 -0500

    Part 6 changes and complete course

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -62,5 +62,68 @@ namespace StarChart.Controllers
 
             return Ok(objects);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]CelestialObject co)
+        {
+            _context.Add(co);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetById", new { id = co.Id }, co);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CelestialObject co)
+        {
+            var currObj = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+
+            if (currObj == null)
+            { 
+                return NotFound();
+            }
+
+            currObj.Name = co.Name;
+            currObj.OrbitalPeriod = co.OrbitalPeriod;
+            currObj.OrbitedObjectId = co.OrbitedObjectId;
+
+            _context.Update(currObj);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name)
+        {
+            var currObj = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+
+            if (currObj == null)
+            {
+                return NotFound();
+            }
+
+            currObj.Name = name;
+
+            _context.Update(currObj);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var matchingObjs = _context.CelestialObjects.Where(x => x.Id == id || x.OrbitedObjectId == id);
+
+            if (!matchingObjs.Any())
+            {
+                return NotFound();
+            }
+
+            _context.RemoveRange(matchingObjs);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
     }
 }
