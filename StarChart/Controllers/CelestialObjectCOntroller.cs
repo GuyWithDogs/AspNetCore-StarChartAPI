56a90a712487bb1ec2c87f7fa0412e4516aa9114
Warrick Wilson <guywithdogs@gmail.com>
2020-05-06 13:10:48 -0500

    Part 4 typo fix but there's a weird issues lurking still..

--- StarChart/Controllers/CelestialObjectCOntroller.cs
+++ StarChart/Controllers/CelestialObjectCOntroller.cs
@@ -9,11 +9,11 @@ namespace StarChart.Controllers
 {
     [Route("")]
     [ApiController]
-    public class CelestialObjectCOntroller: ControllerBase
+    public class CelestialObjectController: ControllerBase
     {
         private readonly ApplicationDbContext _context;
 
-        public CelestialObjectCOntroller(ApplicationDbContext context)
+        public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
         }
