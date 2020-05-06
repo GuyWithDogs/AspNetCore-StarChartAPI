9f5a572fb9bcf05d36ecd2b9cf50f1ea6046488f
Warrick Wilson <guywithdogs@gmail.com>
2020-05-06 13:47:57 -0500

    Part 5 changes

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using StarChart.Data;
+using StarChart.Models;
 
 namespace StarChart.Controllers
 {
@@ -16,6 +18,49 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("{id:int}", Name="GetById")]
+        public IActionResult GetById(int id)
+        {
+            var obj = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            obj.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == id).ToList();
+
+            return Ok(obj);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var obj = _context.CelestialObjects.FirstOrDefault(x => x.Name.Equals(name));
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            obj.Satellites = _context.CelestialObjects.Where(x => x.OrbitedObjectId == obj.Id).ToList();
+
+            var nameMatches = _context.CelestialObjects.Where(x => x.Name.Equals(name));
+
+            return Ok(nameMatches);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var objects = _context.CelestialObjects.AsEnumerable();
+            foreach (var obj in objects)
+            {
+                obj.Satellites = _context.CelestialObjects.Where(x => obj.Id == x.Id).ToList();
+            }
+
+            return Ok(objects);
         }
     }
 }
